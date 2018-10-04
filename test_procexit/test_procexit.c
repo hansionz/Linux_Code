429e41956bef204be68246dc603b64c3c2369c17
Centos6.5 <zsc@localhost.localdomain>
2018-10-04 14:51:59 -0700

    Add test_proexit coding

---
+++ test_procexit/test_procexit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+//int main(){
+//    printf("hello Linux");
+//    exit(0);
+//i}
+
+int main(){
+
+    printf("hello Linux");
+    _exit(0);
+}
