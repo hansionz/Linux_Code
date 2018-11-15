0252d84711698f6e10b6917a6c881a4c22d79bab
hansionz <1776844548@qq.com>
2018-11-15 20:19:06 +0800

    解决MySleep由于时序导致错误的问题

--- MySleep/MySleep.cpp
+++ MySleep/MySleep.cpp
@@ -4,15 +4,24 @@
 using namespace std;
 // handler只需要捕捉它不需要使用它实现功能
 void handler(int no)
-{}
+{
+  //什么也不需要做
+}
 int MySleep(unsigned int second)
 {
   struct sigaction act,oact;
+  sigset_t newmask,oldmask;
   act.sa_handler =handler;
   sigemptyset(&act.sa_mask);
   sigaction(SIGALRM,&act,&oact);
+  sigemptyset(&newmask);
+  sigaddset(&newmask, SIGALRM); //1.屏蔽SIGALRM信号
+
   alarm(second);
-  pause();
+
+  sigdelset(&oldmask, SIGALRM);//2.解除旧的信号屏蔽字对信号SIGALRM的屏蔽
+
+  sigsuspend(&oldmask);//3.解除对信号SIGALRM信号的屏蔽并挂起进程
 
   // 因为前边调用了自定义处理动作，但是如果后边再次使用alarm函数
   // 它的处理动作却是别人自定义的，所以这个调用完成之后要改归来
@@ -26,8 +35,8 @@ int main()
 {
   while(1)
   {
-       MySleep(5);
-       cout << "wake up" << endl;
+       MySleep(3);
+       cout << "wake up done" << endl;
   }
   return 0;
 }
