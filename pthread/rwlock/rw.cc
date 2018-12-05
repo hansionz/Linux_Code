351c27ce4e76aa82d03d279d4ac00acfa9a8260c
hansionz <1776844548@qq.com>
2018-12-05 20:56:36 +0800

    增加读写锁的注释xinxi

--- pthread/rwlock/rw.cc
+++ pthread/rwlock/rw.cc
@@ -6,19 +6,20 @@ using namespace std;
 int book = 0;
 pthread_rwlock_t rwlock;
 
+//可以存在任意多个线程同时读
 void *read_routine(void *arg)
 {
   while(1)
   {
     pthread_rwlock_rdlock(&rwlock);
     
-    cout << "read book data is:" << book << endl;
+    cout << "my tid is:" << pthread_self() << ".read book data is:" << book << endl;
 
     pthread_rwlock_unlock(&rwlock);
     sleep(1);
   }
 }
-
+//任意时刻只能有一个写者线程在写(在写的时候不能有读者读)
 void *write_routine(void *arg)
 {
   while(1)
@@ -26,7 +27,7 @@ void *write_routine(void *arg)
     pthread_rwlock_wrlock(&rwlock);
 
     ++book;
-    cout << "write book data is:" << book << endl;
+    cout << "my tid is:"<< pthread_self() << ".write book data is:" << book << endl;
     
     pthread_rwlock_unlock(&rwlock);
     sleep(2);
@@ -34,9 +35,10 @@ void *write_routine(void *arg)
 }
 int main()
 {
+  //初始化读写锁变量
   pthread_rwlock_init(&rwlock, NULL);
 
-  pthread_t r1,r2,w1,w2;
+  pthread_t r1,r2,w1,w2;//创建两个读者和写者线程
   pthread_create(&r1, NULL, read_routine, NULL);
   pthread_create(&r2, NULL, read_routine, NULL);
   pthread_create(&w1, NULL, write_routine, NULL);
@@ -47,6 +49,7 @@ int main()
   pthread_join(w1, NULL);
   pthread_join(w2, NULL);
 
+  //销毁读写锁
   pthread_rwlock_destroy(&rwlock);
   return 0;
 }
