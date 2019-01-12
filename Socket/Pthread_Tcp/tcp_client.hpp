5150da0c76ab3919a22c48901c9c07a66e88ae16
hansionz <1776844548@qq.com>
2019-01-12 18:18:36 +0800

    修改多线程Tcp服务器

---
+++ Socket/Pthread_Tcp/tcp_client.hpp
@@ -0,0 +1,39 @@
+#pragma once 
+
+#include "tcp_socket.hpp"
+
+class TcpClient
+{
+public:
+  TcpClient(string ip, uint16_t port)
+    :_ip(ip)
+     ,_port(port)
+  {
+    sock.Socket();
+  }
+  void Connect()
+  {
+    sock.Connect(_ip, _port);
+  } 
+  void Recv(string* buf)
+  {
+    sock.Recv(buf);
+  }
+  void Send(const string& buf)
+  {
+    sock.Send(buf);
+  }
+  void Close()
+  {
+    cout << "client quit..." << endl;
+    sock.Close();
+  }
+  ~TcpClient()
+  {
+    sock.Close();
+  }
+private:
+  TcpSocket sock;
+  string _ip;
+  uint16_t _port;
+};
